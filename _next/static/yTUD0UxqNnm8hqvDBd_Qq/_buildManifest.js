8a0b2d0e6016f7ce72ccc3f53b4ffdc778192434
OssFit <oalatrista@gmail.com>
2023-03-26 17:41:18 -0500

    Updates

---
+++ _next/static/yTUD0UxqNnm8hqvDBd_Qq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/cf7c974877e199ec.css","static/chunks/pages/index-f5d29725188dafa5.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
