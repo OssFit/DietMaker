99236e46160d1d7ff0c37c44b41ff4372da54ecc
OssFit <oalatrista@gmail.com>
2023-03-26 17:26:11 -0500

    Clear gh-pages branch

--- README.md
+++
@@ -1,1 +0,0 @@
-Jugando con la API de GPT-3 para evaluar su resultados en un crador de Dietas.
--- out/_next/static/u9TqwQmV3nz5LIeMstVlF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/cf7c974877e199ec.css","static/chunks/pages/index-f5d29725188dafa5.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
